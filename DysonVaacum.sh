36e29d7ff18749016a08dbd81b743c5ce5db9cf2
YoNixNeXRo <46779620+YoNixNeXRo@users.noreply.github.com>
2021-09-03 12:10:03 +0200

    Update DysonVaacum.sh

--- DysonVaacum.sh
+++ DysonVaacum.sh
@@ -2,22 +2,22 @@
 
 
 # Nom 		: dyson.sh
-# Description 	: Aspire sur un site donné un type de documents donné
-# Param 1 	: url du site de type index of
-# Param 2 	: extention des documents a telecharger
-# Param 3	: nombre de documentsa  telecharger
-# Param 4 	:repertoire de destination
+# Description 	: draws on a website the type of wanted documents
+# Param 1 	: url
+# Param 2 	: type of documents (pdf etc)
+# Param 3	: number of wanted documents
+# Param 4 	: destination path
 
 clear 
 
 if [ $# -ne 4 ]
 then 
-	echo "erreur de syntaxe"
-	echo "$0 URL xextension nbfic destination"
-	echo "$0 http://tsoungui.fr/ebooks pdf 7 /home/davy/ebooks_security"
+	echo "syntax error"
+	echo "$0 URL type number path"
+	echo "$0 http://tsoungui.fr/ebooks pdf 7 /home/yonix_nexro/ebooks_security"
 fi
 
 wget  $(lynx -dump -listonly  $1|grep -v /$ |grep "$2$"|cut -c7-|head -$3|sed s/' '/'%20'/g) -P $4 -q --show-progress
 
 echo " "
-echo "$3 fichiers au format .$2 ont été téléchargés"
+echo "$3 .$2 files have been downloaded"
